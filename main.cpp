e6f29c23d1652959ac8db5d317fd88c346eaa0b3
Hualet Wang <mr.asianwang@gmail.com>
2014-08-18 10:11:12 +0800

    qwidget branch initial commit
    Change-Id: Ib488c34074eb192518387f9a5b88d06a7fc5b1e2

--- main.cpp
+++ main.cpp
@@ -1,18 +1,8 @@
-#include <QGuiApplication>
-
-#include "dbus_manager_adaptor.h"
-#include "manager_object.h"
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
+    QApplication a(argc, argv);
 
-    ManagerObject manager;
-    ManagerAdaptor manger(&manager);
-
-    QDBusConnection connection = QDBusConnection::sessionBus();
-    connection.registerService("com.deepin.menu");
-    connection.registerObject("/com/deepin/menu", &manager);
-
-    return app.exec();
+    return a.exec();
 }
